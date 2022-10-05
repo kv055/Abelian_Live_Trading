90417fd72876d4fff4ebf9376d80f8e7a724ff79
Kilian Voss <kilian96@live.de>
2022-10-05 22:27:16 +0200

    logic from Test File was migrated mostly to Deployment Execusion and to Poll Messages

--- Deployment/Deployment_Execution.py
+++ Deployment/Deployment_Execution.py
@@ -1,37 +1,61 @@
-from Message_Que.Q_PUB_SUB_class import Q
+# put the logic of Test File Poll messages in here
+# minus the part where the messages get polled
+
+# if it works then delete functions x y from Poll_Messages.py
+
 from Database_SQL.Querry_Config_Class import config_live_trading
 from Deployment.Create_Connectors_Class import Create_Connectors
 from Deployment.Create_Asset_Dicts_Class import Create_Price_Refresh
 
-class Subscribe_Config_Rows:
+from Message_Que.Poll_Messages_Class import Subscribe_Config_Rows
+
+class Execute_Deployment:
     def __init__(self):
-        self.que = Q()
+        self.que_subscription = Subscribe_Config_Rows()
         self.config_table = config_live_trading() 
-      
-    def stop_trading_messages(self, all_config_rows):
+
+    def stop_trading(
+        self, 
+        all_config_rows, 
+        list_of_deployed_config_row_ids
+        ):
         
-        fetched_messages = self.que.subscribe()
-        if len(fetched_messages) > 0:
+        fetched_messages = self.que_subscription.fetch_stop_trading_messages()
+        if fetched_messages == True:
             for message in fetched_messages:
-                if message['Message_Group'] == 'Stop_Trading':
-                    # filter function
-                    def find_deployed_config_by_id(deployed_strategy):
-                        if deployed_strategy['id'] == message['Config_row_Id']:
-                            return True
-                    # find the deployed strat by the messageid from the stop message 
-                    matched_deployed_configuration =  list(
-                        filter(
-                        find_deployed_config_by_id,
-                        all_config_rows
-                    ))
-                    for config_row in matched_deployed_configuration:
-                        all_config_rows.remove(config_row)
-                    # delete message id from row in the DB
-                    self.config_table.delete_message_id_from_stoped_config_rows(message['Config_row_Id'])
+                # filter function
+                def find_deployed_config_by_id(deployed_strategy):
+                    if deployed_strategy['id'] == message['Config_row_Id']:
+                        return True
+                
+                def find_deployed_id(deployed_config_row_id):
+                    if deployed_config_row_id == message['Config_row_Id']:
+                        return True
 
-                    # # To do: delete message from Q delete message from Q
+                matched_deployed_configuration =  list(
+                    filter(
+                    find_deployed_config_by_id,
+                    all_config_rows
+                ))
 
-    def deploy_config_rows(
+                matched_deployed_id =  list(
+                    filter(
+                    find_deployed_id,
+                    list_of_deployed_config_row_ids
+                ))
+
+                for config_row in matched_deployed_configuration:
+                    all_config_rows.remove(config_row)
+
+                for id in matched_deployed_id:
+                    list_of_deployed_config_row_ids.remove(id)
+                
+                # delete message id from row in the DB
+                self.config_table.delete_message_id_from_stoped_config_rows(message['Config_row_Id'])
+
+                # To do: delete message from Q delete message from Q
+
+    def deploy_trading(
         self,
         config_row_id_list,
         deployed_config_rows,
@@ -41,13 +65,12 @@ class Subscribe_Config_Rows:
         deployed_connectors
         ):
 
-        fetched_messages = self.que.subscribe()
-        if len(fetched_messages) > 0:
+        fetched_messages = self.que_subscription.fetch_deploy_trading_messages()
+        if fetched_messages == True:
             for message in fetched_messages:
-                msg_group = message['Message_Group']
                 config_row = message['Config_Row']
 
-                if msg_group == 'Deploy' and config_row['id'] not in config_row_id_list:
+                if config_row['id'] not in config_row_id_list:
                     config_row_id_list.append(config_row['id'])
 
                     asset_id = config_row['asset_id']
@@ -91,9 +114,3 @@ class Subscribe_Config_Rows:
                     config_row['connector'] = connector
 
                     deployed_config_rows.append(config_row)
-
-
-                    
-
-                    
-
---
+++ Message_Que/Poll_Messages_Class.py
@@ -0,0 +1,25 @@
+from Message_Que.Q_PUB_SUB_class import Q
+
+class Subscribe_Config_Rows:
+    def __init__(self):
+        self.que = Q()
+      
+    def fetch_stop_trading_messages(self):
+        fetched_messages = self.que.subscribe()
+        
+        for message in fetched_messages:
+            if message['Message_Group'] != 'Stop_Trading':
+                fetched_messages.remove(message)
+
+        if len(fetched_messages) > 0:
+            return fetched_messages
+
+    def fetch_deploy_trading_messages(self):
+        fetched_messages = self.que.subscribe()
+        
+        for message in fetched_messages:
+            if message['Message_Group'] != 'Deploy':
+                fetched_messages.remove(message)
+
+        if len(fetched_messages) > 0:
+            return fetched_messages
