2eaa1234950d800053bd8392d55fe73768800720
Kilian Voss <kilian96@live.de>
2022-12-21 11:31:05 +1100

    Basic structure for Deploying a single strat without any que execution

--- App.py
+++ App.py
@@ -1,40 +1,43 @@
 import time
 
-from Deployment.Deployment_Execution import Execute_Deployment
-# from Message_Que.Test_File_Poll_Messages import Subscribe_Config_Rows
+# from Deployment.Deployment_Execution import Execute_Deployment
+
+from Deployment.Create_Connectors_Class import Create_Connectors
+from Deployment.Create_Asset_Dicts_Class import Create_Price_Refresh
+
 from Strategies.DummyStrategy import DumbStrategy
 
 # Connecting to Q
 # Q_Instance = Subscribe_Config_Rows()
-Q_Instance = Execute_Deployment()
+# Q_Instance = Execute_Deployment()
 
-config_row_id_list = []
 all_config_rows = []
-
-asset_id_list = []
-all_asset_url_dicts = []
-
-key_id_list = []
-all_connectors = []
 
 test_strategy = DumbStrategy()
 
+# Get Priv Keys 
+keys_dict = {
+    # ALPACA PAPERTADING KEYS
+    'data_provider':'Alpaca',
+    'pub_key' : 'PK0RPI7F5X8M0VVED26I',
+    'priv_key' :'6VoDwGzKZ3uvPPq8awfspLesuYDnPqYtx0ZhPefG',
+    'api_endpoint': 'https://paper-api.alpaca.markets'
+}
+asseet_dict = {
+    "data_provider": "Alpaca",
+    "ticker": "AAPL",
+    "candleSize": "1_Day"
+}
+connector = Create_Connectors(keys_dict)
+price_data = Create_Price_Refresh(asseet_dict,connector)
+obj = {
+    'pricedata':price_data.fetch_price_data,
+    'connector':connector,
+    'pricedata':{'asset_dict':asseet_dict}
+}
+all_config_rows.append(obj)
 while True:
-
-    Q_Instance.deploy_trading(
-        config_row_id_list,
-        all_config_rows,
-        asset_id_list,
-        all_asset_url_dicts,
-        key_id_list,
-        all_connectors
-    )
-
-    Q_Instance.stop_trading(
-        all_config_rows,
-        config_row_id_list
-    )
-    
+  
     test_strategy.execute_trading(all_config_rows)
 
-    time.sleep(1)
+    time.sleep(600)
