460f1745fdafad6f0c51e619c029c99cc1ac3397
Kilian Voss <kilian96@live.de>
2022-12-26 21:27:59 +1100

    addet logging

--- Strategies/DummyStrategy.py
+++ Strategies/DummyStrategy.py
@@ -30,7 +30,8 @@ class DumbStrategy:
             # Signal Execution
             if (n >= 0.8):
                 order_dict['side'] = 'buy'
-                account_cursor.new_order(order_dict)
             elif(n <= 0.2):
                 order_dict['side'] = 'sell'
-                account_cursor.new_order(order_dict)
+
+            order_confirmation_or_rejection = account_cursor.new_order(order_dict)
+            print(order_confirmation_or_rejection)
