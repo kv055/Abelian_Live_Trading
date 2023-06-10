a4bb9facdaec35442a05b13e09c0f1a5b3d3dd0b
Kilian Voss <kilian96@live.de>
2023-06-10 15:46:18 +1000

    code cleanup

--- App.py
+++ App.py
@@ -1,38 +1,51 @@
-import time
+import os
+import load_dotenv
 
 from Deployment.Create_Connectors_Class import Create_Connectors
 from Deployment.Create_Asset_Dicts_Class import Create_Price_Refresh
-
 from Strategies.DummyStrategy import DumbStrategy
 
+# INITIALIZE STRATEGY
 all_config_rows = []
-
 test_strategy = DumbStrategy()
 
-# Get Priv Keys 
+# Get Priv Keys
+load_dotenv()
+pub_key = os.getenv('BINANCE_PUB')
+priv_key = os.getenv('BINANCE_PRIV')
+api_endpoint = os.getenv('BINANCE_API_END')
+
 keys_dict = {
-    # ALPACA PAPERTADING KEYS
-    'data_provider':'Alpaca',
-    'pub_key' : 'PK0RPI7F5X8M0VVED26I',
-    'priv_key' :'6VoDwGzKZ3uvPPq8awfspLesuYDnPqYtx0ZhPefG',
-    'api_endpoint': 'https://paper-api.alpaca.markets'
+    # BINANCE KEYS
+    'data_provider': 'Binance',
+    'pub_key': pub_key,
+    'priv_key': priv_key,
+    'api_endpoint': api_endpoint
 }
 asseet_dict = {
-    "data_provider": "Alpaca",
-    "ticker": "AAPL",
-    "candleSize": "1_Day"
+    "data_provider": "Binance",
+    "ticker": "BTCUSDC",
+    "candleSize": "1_Hour"
 }
+
+# INITIALIZE CONNECTOR OBJECT
 connector = Create_Connectors(keys_dict)
-price_data = Create_Price_Refresh(asseet_dict,connector)
+
+# INITIALIZE PRICE DATA OBJECT
+price_data = Create_Price_Refresh(asseet_dict, connector)
+
+# CREATE TRADING OBJECT
 obj = {
-    'pricedata':price_data.fetch_price_data,
-    'connector':connector,
-    'asset_dict':asseet_dict
+    'pricedata': price_data.fetch_price_data,
+    'connector': connector,
+    'asset_dict': asseet_dict
 }
 all_config_rows.append(obj)
 
-while True:
-  
+
+def lambda_handler(event, context):
     test_strategy.execute_trading(all_config_rows)
-    print('Sleeping for 600 Seconds')
-    time.sleep(600)
+    return {
+        'statusCode': 200,
+        'body': 'Execution completed'
+    }
