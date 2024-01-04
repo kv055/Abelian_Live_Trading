f51c0ea482f1bcf9a5ee10626ede03511fbefab3
Kilian Voss <kilianvoss@proton.me>
2024-01-04 09:15:09 +0000

    Dataformatig (float to str) before writing to dynamo, fixed boto client init

--- Database_Dynamo/aws_dynamo_connect.py
+++ Database_Dynamo/aws_dynamo_connect.py
@@ -12,19 +12,27 @@ class DynamoDB_Class:
             self.AWSDYNAMO_TABLE = os.getenv('AWSDYNAMO_TABLE')
             
             # Initialize a DynamoDB client
-            self.dynamodb = boto3.resource('dynamodb', region_name=self.AWSDYNAMO_REGION)
+            self.dynamodb = boto3.resource(
+                'dynamodb', 
+                region_name=self.AWSDYNAMO_REGION, 
+                aws_access_key_id=self.AWSDYNAMO_KEY_ID, 
+                aws_secret_access_key=self.AWSDYNAMO_PRIV_KEY
+            )
             self.table = self.dynamodb.Table(self.AWSDYNAMO_TABLE)
         except Exception as e:
             print(f"Error during initialization: {e}")
             # You might want to add additional handling (logging, raising exceptions) based on your use case.
 
     def Strategy_Output_Log(self, Strategy_Output):
-        try:
-            self.table.put_item(Item=Strategy_Output)
-            print("Strategy output inserted successfully.")
-        except Exception as e:
-            print(f"Error inserting strategy output: {e}")
-            # Handle the exception appropriately (logging, retrying, etc.).
+        # Convert all floats to str
+        Strategy_Output['Trade_Signal'] = str(Strategy_Output['Trade_Signal'])
+        Strategy_Output['Asset_Price']['last_price'] = str(Strategy_Output['Asset_Price']['last_price'] )
+        Strategy_Output['Order_Sent']['quantity'] = str(Strategy_Output['Order_Sent']['quantity'])
+        Strategy_Output['TradeID'] = Strategy_Output['Trade_Signal'] 
+        
+        self.table.put_item(Item=Strategy_Output)
+        l = 0
+
 
     def Liver_Orders_Log(self, Order):
         try:
